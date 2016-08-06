3f0467e2dba3089fb288079d174df7c6ba53357f
Rubèn Nieto <ruben.np@me.com>
2016-08-06 16:14:32 +0200

    Initialize commit

---
+++ app/app.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');  // ES6 sintax equal to var Route = require('react-router').Route; etc...
+
+// Load foundation
+
+jQuery(document).ready(function($) {
+  $(document).foundation();
+}); // $(document).foundation();
+
+// App css
+require('style!css!sass!applicationStyles');
+
+ReactDOM.render(
+  <p>React Boilerplate Project</p>,
+  document.getElementById('app')
+);
