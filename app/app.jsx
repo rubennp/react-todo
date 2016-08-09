54e08040644c523d6faf9e4f67048ee2b9b6bec1
Rubèn Nieto <ruben.np@me.com>
2016-08-09 20:19:43 +0200

    Refactor TodoList, Todo and AddTodo to work with redux

--- app/app.jsx
+++ app/app.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var {Provider} = require('react-redux');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');  // ES6 sintax equal to var Route = require('react-router').Route; etc...
 
 var TodoApp = require('TodoApp');
@@ -25,6 +26,8 @@ jQuery(document).ready(function($) {
 require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
-  <TodoApp />,
+  <Provider store={store}>
+    <TodoApp />
+  </Provider>,
   document.getElementById('app')
 );
--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -1,58 +1,31 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var {Provider} = require('react-redux');
+
 var expect = require('expect');
 var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
 
+var configureStore = require('configureStore');
 var TodoApp = require('TodoApp');
+import TodoList from 'TodoList';
 
 describe('TodoApp', () => {
   it('should exist', () => {
     expect(TodoApp).toExist();
   });
 
-  it('should add todo to the todos state on handleAddTodo', () => {
-    var todoText = 'test text';
-    var todoApp = TestUtils.renderIntoDocument(<TodoApp />);
+  it('should render TodoList', () => {
+    var store = configureStore.configure();
+    var provider = TestUtils.renderIntoDocument(
+      <Provider store={store}>
+        <TodoApp/>
+      </Provider>
+    );
 
-    todoApp.setState({todos: []});
-    todoApp.handleAddTodo(todoText);
+    var todoApp = TestUtils.scryRenderedComponentsWithType(provider, TodoApp)[0];
+    var todoList = TestUtils.scryRenderedComponentsWithType(todoApp, TodoList);
 
-    expect(todoApp.state.todos[0].text).toBe(todoText);
-    expect(todoApp.state.todos[0].createdAt).toBeA('number');
-  });
-
-  it('should toggle completed value when handleToggle called', () => {
-    var todoData = {
-      id: 11,
-      text: 'Test features',
-      completed: false,
-      createdAt: 0,
-      completedAt: undefined
-    };
-    var todoApp = TestUtils.renderIntoDocument(<TodoApp />);
-    todoApp.setState({todos: [todoData]});
-
-    expect(todoApp.state.todos[0].completed).toBe(false); // check todos first item has completed value of false
-    todoApp.handleToggle(11);                             // call handleToggle with 11
-    expect(todoApp.state.todos[0].completed).toBe(true);  // verify that value changed
-    expect(todoApp.state.todos[0].completedAt).toBeA('number');
-  });
-
-  it('should toggle todo from completed to incompleted', () => {
-    var todoData = {
-      id: 11,
-      text: 'Test features',
-      completed: true,
-      createdAt: 0,
-      completedAt: 123
-    };
-    var todoApp = TestUtils.renderIntoDocument(<TodoApp />);
-    todoApp.setState({todos: [todoData]});
-
-    expect(todoApp.state.todos[0].completed).toBe(true); // check todos first item has completed value of false
-    todoApp.handleToggle(11);                             // call handleToggle with 11
-    expect(todoApp.state.todos[0].completed).toBe(false);  // verify that value changed
-    expect(todoApp.state.todos[0].completedAt).toNotExist();
+    expect(todoList.length).toEqual(1);
   });
 });
